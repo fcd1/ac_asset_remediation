f91e70b5abd9282e24c730e27bfd1f949dc47e10
Fred Duby <fcd1@columbia.edu>
2017-03-20 17:22:28 -0400

    Remove raise statement used for debugging

--- ac_asset_remediation.rb
+++ ac_asset_remediation.rb
@@ -32,7 +32,7 @@ puts "Repository url is set to #{repoinfo[:url]}"
 puts "Is this correct? If so, please type in 'Yes' to continue with the script"
 response = gets.chomp
 raise "User aborted script by not entering 'Yes'" unless response.eql?('Yes')
-raise "User Answered 'Yes'"
+puts "User Answered 'Yes'"
 repo = Rubydora.connect url: repoinfo[:url], user: repoinfo[:user], password: repoinfo[:password]
 puts "Using Fedora Commons instance location at #{repo.config[:url]}"
 
