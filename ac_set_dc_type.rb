4a6e9761989bf89f1b30d1dd4cd3717cfc68b9d4
Fred Duby <fcd1@columbia.edu>
2017-04-04 14:39:11 -0400

    Add new script to set DC type, and handle pid not found exception

--- ac_set_dc_type.rb
+++ ac_set_dc_type.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/hash'
 require 'rubydora'
 require_relative 'datastream_transformation'
 
-puts 'Getting requested info for the given fedora pids'
+puts 'Set DC type for the given fedora pids'
 
 # function definitions
 
@@ -13,7 +13,7 @@ puts 'Getting requested info for the given fedora pids'
 # the yaml file -- these format changes would be localized
 # to this method
 def pids
-  Array.new YAML.load_file('get_info_fedora_object_pids.yml')
+  Array.new YAML.load_file('set_dc_type_fedora_object_pids.yml')
 end
 
 # add module AssetRemediation to Rubydora::DigitalObject
@@ -24,10 +24,17 @@ end
 
 # read configs from yaml file. Complain if config is missing
 # change the config filename to default to name in code but sup
-CONFIG =  ActiveSupport::HashWithIndifferentAccess.new YAML.load_file('get_info_config.yml')
+CONFIG =  ActiveSupport::HashWithIndifferentAccess.new YAML.load_file('set_dc_type_config.yml')
 
 # read in the hyacinth allowed types for dc:type
 ALLOWED_DC_TYPES = Array.new CONFIG[:hyacinth][:allowed_dc_types]
+
+# read in mapping between CONTENT mime type and Hyacinth-approved DC type
+MIME_TYPE_TO_DC_TYPE_MAPPING =
+  ActiveSupport::HashWithIndifferentAccess.new YAML.load_file('mime_type_to_dc_type_mapping.yml')
+puts 'Mapping starts here'
+puts MIME_TYPE_TO_DC_TYPE_MAPPING.inspect
+puts 'Mapping ends here'
 
 raise 'url of repository missing' unless CONFIG.has_key? :fedora_repository
 raise 'user for repository missing' unless CONFIG[:fedora_repository].has_key? :user
@@ -53,10 +60,9 @@ pids.each do |pid|
     next
   end
 
-  puts "Processing fedora object #{ac_obj.pid}, DC Type currently set to #{ac_obj.get_dc_type}"
-  puts "Processing fedora object #{ac_obj.pid}, DC Format currently set to #{ac_obj.get_dc_format}"
-  puts "Processing fedora object #{ac_obj.pid}, mime type for CONTENT DS set to #{ac_obj.get_ds_mime_type 'CONTENT'}"
-  puts "Processing fedora object #{ac_obj.pid}, label for CONTENT DS set to #{ac_obj.get_ds_label 'CONTENT'}"
+  # puts "Processing fedora object #{ac_obj.pid}, DC Type currently set to #{ac_obj.get_dc_type}"
+  # puts "Set DC type to Sound"
+  # ac_obj.set_dc_type 'Sound'
+  # puts "Processing fedora object #{ac_obj.pid}, DC Type currently set to #{ac_obj.get_dc_type}"
   puts
 end
-
