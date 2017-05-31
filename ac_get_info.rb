2003c3b6f1112c74a88c53b6ac28769f32731c74
Fred Duby <fcd1@columbia.edu>
2017-05-31 13:48:07 -0400

    Refactor functionality into class

--- ac_get_info.rb
+++ ac_get_info.rb
@@ -6,67 +6,86 @@ require 'rubydora'
 require_relative 'datastream_transformation'
 
 puts 'This script retrieves info for the given fedora pids'
+puts
 
-# function definitions
+class GetInfoProcessing
 
-# following is factored out as function in case the
-# we decide to change format of the information within
-# the yaml file -- these format changes would be localized
-# to this method
-def pids
-  Array.new YAML.load_file('get_info_fedora_object_pids.yml')
-end
+  attr_reader :options, :pids_by_file, :args, :repo_info
 
-def process_command_line_options_and_args(options = {}, args = {})
-
-  # First, process options. Will use OptionParser.parse!, which removes
-  # switches destructively. Non-option arguments remaun in argv
-
-  OptionParser.new do |opts|
-    opts.banner = "Usage: put stuff in here"
-
-    opts.on("-fINPUTFILE", "--input_file=INPUTFILE", "yml file containing pids", Array) do |f|
-      # fcd1, 05/26/17: for now, handle just one input file
-      options[:input_file] = f
-    end
-
-  end.parse!
-
-  args = ARGV
-
-end
-
-def process_repo_config
-end
-
-def process_pid_files(options = {}, pids = {})
-  options[:input_file].each do |filename|
-    raise "File does not exist" unless File.file?(filename)
-    pids[filename] = Array.new YAML.load_file(filename)
-    puts "File is #{filename}"
+  def initialize
+    @options = {}
+    @pids_by_file = {}
   end
-  pids
-end
 
-def process_pids(repo, pids)
+  def process_command_line_options_and_args
 
-  # process each object
-  pids.each do |pid|
-    puts "Looking for fedora object with pid #{pid}"
+    # First, process options. Will use OptionParser.parse!, which removes
+    # switches destructively. Non-option arguments remaun in argv
 
-    begin
-      ac_obj = repo.find(pid)
-    rescue Rubydora::RecordNotFound
-      puts '************************ PID not found ***********************'
-      next
+    OptionParser.new do |opts|
+      opts.banner = "Usage: put stuff in here"
+
+      opts.on("-fINPUTFILE", "--input_file=INPUTFILE", "yml file containing pids", Array) do |f|
+        # fcd1, 05/26/17: for now, handle just one input file
+        @options[:input_files] = f
+      end
+
+    end.parse!
+
+    @args = ARGV
+
+  end
+
+  def process_repo_config
+    # read configs from yaml file. Complain if config is missing
+    # change the config filename to default to name in code but sup
+    @config_info =  ActiveSupport::HashWithIndifferentAccess.new YAML.load_file('get_info_config.yml')
+
+    # read in the hyacinth allowed types for dc:type
+    @allowed_dc_types = Array.new @config_info[:hyacinth][:allowed_dc_types]
+
+    raise 'url of repository missing' unless @config_info.has_key? :fedora_repository
+    raise 'user for repository missing' unless @config_info[:fedora_repository].has_key? :user
+    raise 'password for repository missing' unless @config_info[:fedora_repository].has_key? :password
+
+    @repo_info = @config_info[:fedora_repository]
+  end
+
+  def connect_to_repo
+
+    @repo = Rubydora.connect url: @repo_info[:url], user: @repo_info[:user], password: @repo_info[:password]
+    puts "Using Fedora Commons instance location at #{@repo.config[:url]}"
+
+  end
+
+  def process_pid_files
+    @options[:input_files].each do |filename|
+      raise "File does not exist" unless File.file?(filename)
+      @pids_by_file[filename] = Array.new YAML.load_file(filename)
+    end
+  end
+
+  def process_pids(pids)
+
+    # process each object
+    pids.each do |pid|
+      puts "Looking for fedora object with pid #{pid}"
+
+      begin
+        ac_obj = @repo.find(pid)
+      rescue Rubydora::RecordNotFound
+        puts '************************ PID not found ***********************'
+        next
+      end
+
+      puts "Processing fedora object #{ac_obj.pid}, DC Type currently set to #{ac_obj.get_dc_type}"
+      puts "Processing fedora object #{ac_obj.pid}, DC Format currently set to #{ac_obj.get_dc_format}"
+      puts "Processing fedora object #{ac_obj.pid}, mime type for CONTENT DS set to #{ac_obj.get_ds_mime_type 'CONTENT'}"
+      puts "Processing fedora object #{ac_obj.pid}, label for CONTENT DS set to #{ac_obj.get_ds_label 'CONTENT'}"
+      puts "Processing fedora object #{ac_obj.pid}, datastreams are #{ac_obj.datastreams.keys}"
+      puts
     end
 
-    puts "Processing fedora object #{ac_obj.pid}, DC Type currently set to #{ac_obj.get_dc_type}"
-    puts "Processing fedora object #{ac_obj.pid}, DC Format currently set to #{ac_obj.get_dc_format}"
-    puts "Processing fedora object #{ac_obj.pid}, mime type for CONTENT DS set to #{ac_obj.get_ds_mime_type 'CONTENT'}"
-    puts "Processing fedora object #{ac_obj.pid}, label for CONTENT DS set to #{ac_obj.get_ds_label 'CONTENT'}"
-    puts "Processing fedora object #{ac_obj.pid}, datastreams are #{ac_obj.datastreams.keys}"
-    puts
   end
 
 end
@@ -79,45 +98,34 @@ class Rubydora::DigitalObject
 end
 
 # Main Processing
+gip = GetInfoProcessing.new
 
-args = {}
-options = {}
-process_command_line_options_and_args( options, args)
-puts "Called process_command_line_options_and_args"
-puts "Content of options: #{options.inspect}"
-puts "Content of ARGV: #{ARGV.inspect}"
+gip.process_command_line_options_and_args
+# puts "Called #process_command_line_options_and_args"
+# puts "Content of options: #{gip.options.inspect}"
+# puts "Content of ARGV: #{gip.args.inspect}"
 
-pids_by_file = process_pid_files options
-puts "Called process_pid_files"
-puts pids_by_file.inspect
+gip.process_repo_config
 
-# read configs from yaml file. Complain if config is missing
-# change the config filename to default to name in code but sup
-CONFIG =  ActiveSupport::HashWithIndifferentAccess.new YAML.load_file('get_info_config.yml')
+gip.process_pid_files
+# puts "Called process_pid_files"
+# puts gip.pids_by_file.inspect
 
-# read in the hyacinth allowed types for dc:type
-ALLOWED_DC_TYPES = Array.new CONFIG[:hyacinth][:allowed_dc_types]
-
-raise 'url of repository missing' unless CONFIG.has_key? :fedora_repository
-raise 'user for repository missing' unless CONFIG[:fedora_repository].has_key? :user
-raise 'password for repository missing' unless CONFIG[:fedora_repository].has_key? :password
-
-repoinfo = CONFIG[:fedora_repository]
-puts "Repository url is set to #{repoinfo[:url]}"
+puts "Repository url is set to #{gip.repo_info[:url]}"
 puts "You have 5 seconds to interrupt this script (Using ctl-c)"
 (0..5).each do |i|
   print "#{i}.."
   sleep 1
 end
+puts
 puts 'Getting requested info for the given fedora pids'
 
-repo = Rubydora.connect url: repoinfo[:url], user: repoinfo[:user], password: repoinfo[:password]
-puts "Using Fedora Commons instance location at #{repo.config[:url]}"
+gip.connect_to_repo
 
-pids_by_file.each  do |filename, pid_array|
+gip.pids_by_file.each  do |filename, pid_array|
   puts "About to process pids from file #{filename}"
-  puts pid_array.inspect
-  process_pids(repo, pid_array)
+  # puts pid_array.inspect
+  gip.process_pids pid_array
 end
 
 
